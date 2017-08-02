0dbf721040c9c8432f6895ec16fded559f337720
hanjx <511572653@qq.com>
2017-08-02 09:48:13 +0800

    Initial commit

---
+++ src/components/Mine.vue
@@ -0,0 +1,36 @@
+<template>
+   <div>
+   	 <img id="code" src="../img/code.png">
+   	 <div id="note" v-once>{{msg}}<div id="h"></div></div>
+
+   </div>
+
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg: 'vue.js正式入门'
+    }
+  }
+}
+</script>
+
+<style >
+#code{
+	width: 80%;
+	margin:10px 10%;
+
+}
+#note{
+	width: 80%;
+	line-height: 35px;
+	margin: 10px auto;
+
+}
+ #h{
+ 	width: 100%;
+ 	height: 60px;
+ }
+</style>
---
+++ src/components/Role.vue
@@ -0,0 +1,37 @@
+<template>
+<div id="role">
+	<div id="title">特惠说明</div>
+		<ul>
+			<li>1.学习使用vue cli</li>
+			<li>2.学习mvvm思想</li>
+			<li>3.学习vue组件化</li>
+		</ul>
+	</div>
+</div>
+</template>
+<script>
+
+
+</script>
+<style>
+	#role{
+		width: 100%;
+
+	}
+	#title{
+		width: 100%;
+		text-align: center;
+		line-height: 40px;
+		color: red;
+	}
+	ul{
+     width: 100%;
+     list-style: none;
+	}
+   ul li{
+        width: 90%;
+        margin-left: 5%;
+        line-height: 40px;
+		color: #acd000;
+	}
+</style>
